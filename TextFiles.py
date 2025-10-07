9af5a107489be5131e134751f95c392e2edb4733
Zoe Kasules <zoekasules@Zoes-MacBook-Air.local>
2025-10-07 13:36:10 -0500

    refactor text files

--- TextFiles.py
+++ TextFiles.py
@@ -1,146 +1,32 @@
 import PyPDF2
 
-with open('sb_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
+def pdf_to_text(pdf_filename, text_filename):
+    with open(pdf_filename, 'rb') as pdf_file:
+        reader = PyPDF2.PdfReader(pdf_file)
+        text = ""
 
-    # PDF files complete 
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('sb_records.txt', 'w', encoding='utf-8') as text_file:
+        for page in reader.pages:
+            text += page.extract_text() + "/n"
+        with open(text_filename, 'w', encoding='utf-8') as text_file:
             text_file.write(text)
-            print("PDF content has been successfully extracted to sb_records.txt")
+            print(f"PDF content has been successfully extracted to {text_filename}")
 
-with open('bsb_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
+files = [
+    ('sb_records.pdf', 'sb_records.txt'),
+    ('bsb_records.pdf', 'bsb_records.txt'),
+    ('Cross_Country_Records.pdf', 'Cross_Country_Records.txt'),
+    ('fball_records.pdf', 'fball_records.txt'),
+    ('mbb_records.pdf', 'mbb_records.txt'),
+    ('mens_tennis_records.pdf', 'mens_tennis_records.txt'),
+    ('mhoc_records.pdf', 'mhoc_records.txt'),
+    ('msoc_records.pdf', 'msoc_records.txt'),
+    ('vball_records.pdf', 'vball_records.txt'),
+    ('wbb_records.pdf', 'wbb_records.txt'),
+    ('whoc_records.pdf', 'whoc_records.txt'),
+    ('wsoc_records.pdf', 'wsoc_records.txt'),
+    ('wtennis_records.pdf', 'wtennis_records.txt')
+]
 
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
+for pdf_file, text_file in files:
+    pdf_to_text(pdf_file, text_file)
 
-        with open('bsb_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to bsb_records.text")
-
-with open('Cross_Country_Records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('Cross_Country_Records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to Cross_Country_Records.txt")
-
-with open ('fball_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('fball_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to fball_records.txt")
-
-with open('mbb_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open ('mbb_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to mbb_records.txt")
-
-with open('mens_tennis_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('mens_tennis_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to mens_tennis_records.txt")
-
-with open('mhoc_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('mhoc_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to mhoc_records.txt")
-
-with open('msoc_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('msoc_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to msoc_records.txt")
-
-with open('vball_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('vball_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to vball_records.txt")
-
-with open('wbb_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('wbb_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to wbb_records.txt")
-
-with open('whoc_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('whoc_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to whoc_records.txt")
-
-with open('wsoc_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('wsoc_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to wsoc_records.txt")
-
-with open('wtennis_records.pdf', 'rb') as pdf_file:
-    reader = PyPDF2.PdfReader(pdf_file)
-    text = ""
-
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-
-        with open('wtennis_records.txt', 'w', encoding='utf-8') as text_file:
-            text_file.write(text)
-            print("PDF content has been successfully extracted to wtennis_records.txt")
