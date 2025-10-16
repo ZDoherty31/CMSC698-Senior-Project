f1c8ef8a1cb8816af1ce8825ba0f441fbbfb4c70
Zoe Doherty <zoedoherty31@gmail.com>
2025-10-16 16:10:29 -0500

    Initial Commit

---
+++ Senior Project/Senior_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Senior_ProjectApp.swift
+//  Senior Project
+//
+//  Created by Zoe Kasules on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Senior_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Senior ProjectTests/Senior_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Senior_ProjectTests.swift
+//  Senior ProjectTests
+//
+//  Created by Zoe Kasules on 10/16/25.
+//
+
+import Testing
+@testable import Senior_Project
+
+struct Senior_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
